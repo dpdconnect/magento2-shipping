c5b0d8208276ca47bf6349f1bce3f5e8f3a92b57
DPD Development <pluginsupport@dpd.nl>
2026-02-24 14:11:52 +0100

    refactor: improve shipment product type logic
    Simplifies conditional checks for shipment product codes by extracting data into variables and restructuring the logic flow
    
    Handles default product type separately from other types and improves code readability

--- Helper/Services/OrderConvertService.php
+++ Helper/Services/OrderConvertService.php
@@ -94,12 +94,17 @@ class OrderConvertService extends AbstractHelper
             }
         }
 
-        // Fetch the code from the shipment, if any, else default to the order code
-        if ($shipment && !empty($shipment->hasData(Constants::SHIPMENT_EXTRA_DATA)['code'])) {
-            return $shipment->getData(Constants::SHIPMENT_EXTRA_DATA)['code'] === '6' ? 'B2C' : $shipment->getData(Constants::SHIPMENT_EXTRA_DATA)['code'];
+        $extraData = $shipment->getData(Constants::SHIPMENT_EXTRA_DATA);
+        $productType = $extraData['productType'] ?? null;
+        $productCode = $extraData['code'] ?? '';
 
+        if ($productType === 'default') {
+            if ($productCode === '6') {
+                return 'B2C';
+            }
+            return empty($productCode) ? 'default' : $productCode;
         }
-
+        
         return $order->getDpdShippingProduct();
     }
 
